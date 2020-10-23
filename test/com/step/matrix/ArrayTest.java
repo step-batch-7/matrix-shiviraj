4c8a27385b3a26db01a20e4bacd29491b30951e1
Shivam Rajput <58025059+shiviraj@users.noreply.github.com>
2020-10-23 15:29:19 +0530

    |Modified test

--- test/com/step/matrix/ArrayTest.java
+++ test/com/step/matrix/ArrayTest.java
@@ -1,6 +1,6 @@
 package com.step.matrix;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -27,14 +27,14 @@ public class ArrayTest{
   public void equals(){
     int[] numbers = {1,2,3};
     Array array = new Array(numbers);
-    assertEquals("Should give true for equal arrays", true, this.array.equals(array));
+    assertTrue("Should give true for equal arrays", this.array.equals(array));
   }
 
   @Test
   public void shouldGiveFalseForTwoUnequalsArray(){
     int[] numbers = {1,2};
     Array array = new Array(numbers);
-    assertEquals("Should give false for unequal array", false, this.array.equals(array));
+    assertFalse("Should give false for unequal array", this.array.equals(array));
   }
 
 }
--- test/com/step/matrix/MatrixTest.java
+++ test/com/step/matrix/MatrixTest.java
@@ -1,19 +1,22 @@
 package com.step.matrix;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 import org.junit.Before;
 import org.junit.Test;
 
 public class MatrixTest{
   private Matrix matrix1;
   private Matrix matrix2;
+  private Matrix matrix3;
 
   @Before
   public void setUpMatrix() {
     int[][] numbers1 = {{1,2,3},{3,4,5},{1,2,3}};
     int[][] numbers2 = {{1,2,3},{3,4,5},{1,2,3}};
+    int[][] numbers3 = {{1,2,3},{3,4,5}};
     this.matrix1 = new Matrix(numbers1);
     this.matrix2 = new Matrix(numbers2);
+    this.matrix3 = new Matrix(numbers3);
   }
 
   @Test
@@ -25,11 +28,25 @@ public class MatrixTest{
   }
 
   @Test
+  public void shouldNotCalculateSumOfUnEqual(){
+    int[][] numbers3 = {{2,4,6},{6,8,10},{2,4,6}};
+    Matrix actual = this.matrix1.add(this.matrix3);
+    assertNull("Should not add two matrix", actual);
+  }
+
+  @Test
   public void subtractMatrix(){
     int[][] numbers3 = {{0,0,0},{0,0,0},{0,0,0}};
     Matrix actual = this.matrix1.subtract(this.matrix2);
     Matrix expected = new Matrix(numbers3);
     assertEquals("Should subtract one matrix from another matrix", expected, actual);
+  }
+
+  @Test
+  public void shouldNotCalculateSubtractOfUnEqual(){
+    int[][] numbers3 = {{2,4,6},{6,8,10},{2,4,6}};
+    Matrix actual = this.matrix1.subtract(this.matrix3);
+    assertNull("Should not add two matrix", actual);
   }
 
   @Test
